a5a22126accc74c041c12b3c98e1774410fcdcf5
Robert Obrochta <obrochtarobert13@gmail.com>
2023-04-05 18:54:27 -0500

    Update settings.py

--- homework/settings.py
+++ homework/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
