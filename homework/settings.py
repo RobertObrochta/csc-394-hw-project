d1b5fbcfda42e6234d927c400764204568919c75
Robert Obrochta <obrochtarobert13@gmail.com>
2023-04-05 18:56:23 -0500

    Update settings.py

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#-g2un%y%s!$*$x4lv0hbo35v6ujguc7!j!+$+*#-7uw!%%epn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
